937ce45483483875145327189d2fa358afa6ba77
k0siara <patryk.kosieradzki@gmail.com>
2019-03-29 20:41:25 +0100

    custom cookie

---
+++ src/main/java/com/patrykkosieradzki/todo/ui/component/CustomCookieConsent.java
@@ -0,0 +1,11 @@
+package com.patrykkosieradzki.todo.ui.component;
+
+import com.vaadin.flow.component.cookieconsent.CookieConsent;
+
+public class CustomCookieConsent extends CookieConsent {
+
+    public CustomCookieConsent() {
+        setPosition(Position.BOTTOM);
+        setDismissLabel("Cool!");
+    }
+}
