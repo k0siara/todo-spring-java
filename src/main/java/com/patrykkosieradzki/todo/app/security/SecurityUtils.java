5c44adb4c9ef862cd57f028ab051bbd10226d6dc
k0siara <patryk.kosieradzki@gmail.com>
2019-03-29 18:44:43 +0100

    disabled login page after successful login

--- src/main/java/com/patrykkosieradzki/todo/app/security/SecurityUtils.java
+++ src/main/java/com/patrykkosieradzki/todo/app/security/SecurityUtils.java
@@ -2,6 +2,9 @@ package com.patrykkosieradzki.todo.app.security;
 
 import com.vaadin.flow.server.ServletHelper;
 import com.vaadin.flow.shared.ApplicationConstants;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.stream.Stream;
@@ -9,6 +12,16 @@ import java.util.stream.Stream;
 public class SecurityUtils {
 
     private SecurityUtils() {}
+
+    public static boolean isUserLoggedIn() {
+        return isUserLoggedIn(SecurityContextHolder.getContext().getAuthentication());
+    }
+
+    private static boolean isUserLoggedIn(Authentication authentication) {
+        return authentication != null
+                && !(authentication instanceof AnonymousAuthenticationToken);
+    }
+
 
     static boolean isFrameworkInternalRequest(HttpServletRequest request) {
         final String parameterValue = request.getParameter(ApplicationConstants.REQUEST_TYPE_PARAMETER);
--- src/main/java/com/patrykkosieradzki/todo/ui/LoginView.java
+++ src/main/java/com/patrykkosieradzki/todo/ui/LoginView.java
@@ -1,50 +1,52 @@
 package com.patrykkosieradzki.todo.ui;
 
+import com.patrykkosieradzki.todo.app.security.SecurityUtils;
 import com.patrykkosieradzki.todo.ui.component.MyCookieConsent;
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.login.LoginI18n;
 import com.vaadin.flow.component.login.LoginOverlay;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.page.Viewport;
 import com.vaadin.flow.router.*;
 import com.vaadin.flow.templatemodel.TemplateModel;
 
 @Route
-@HtmlImport("styles/shared-styles.html")
+@PageTitle("Todo App")
+@Viewport("width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes")
 public class LoginView extends VerticalLayout implements AfterNavigationObserver, BeforeEnterObserver {
 
-    private LoginOverlay login = new LoginOverlay();
+    private LoginOverlay login;
 
     public LoginView() {
-        getElement().appendChild(
-                new MyCookieConsent().getElement(), login.getElement());
-
-        LoginOverlay component = new LoginOverlay();
+        add(new MyCookieConsent());
 
         LoginI18n i18n = LoginI18n.createDefault();
         i18n.setHeader(new LoginI18n.Header());
         i18n.getHeader().setTitle("Todo Application");
         i18n.setAdditionalInformation(null);
 
-        component.setI18n(i18n);
-        component.setForgotPasswordButtonVisible(false);
-        component.setAction("login");
-        component.setOpened(true);
+        login = new LoginOverlay();
+        login.setI18n(i18n);
+        login.setForgotPasswordButtonVisible(false);
+        login.setAction("login");
+        login.setOpened(true);
+
+        add(login);
     }
 
     @Override
     public void beforeEnter(BeforeEnterEvent event) {
-
+        if (SecurityUtils.isUserLoggedIn()) {
+            // Needed manually to change the URL because of https://github.com/vaadin/flow/issues/4189
+            UI.getCurrent().getPage().getHistory().replaceState(null, "");
+            event.rerouteTo(DashboardView.class);
+            login.setOpened(false);
+        }
     }
 
     @Override
     public void afterNavigation(AfterNavigationEvent event) {
-        login.setError(
-                event.getLocation().getQueryParameters().getParameters().containsKey(
-                        "error"));
-    }
-
-    public interface Model extends TemplateModel {
-
-        void setError(boolean error);
+        login.setError(event.getLocation().getQueryParameters().getParameters().containsKey("error"));
     }
 }
