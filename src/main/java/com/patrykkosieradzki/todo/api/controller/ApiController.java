d9dab2373f5a7d76b4b401ddf76f8f1e85a491ef
k0siara <patryk.kosieradzki@gmail.com>
2019-06-06 01:23:14 +0200

    method security update

--- src/main/java/com/patrykkosieradzki/todo/api/controller/ApiController.java
+++ src/main/java/com/patrykkosieradzki/todo/api/controller/ApiController.java
@@ -2,21 +2,23 @@ package com.patrykkosieradzki.todo.api.controller;
 
 import com.patrykkosieradzki.todo.AppConstants;
 import com.patrykkosieradzki.todo.api.entity.ApiInfo;
-import com.patrykkosieradzki.todo.backend.util.ServerUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.servlet.http.HttpServletRequest;
-import java.net.MalformedURLException;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
+    @Value("${server.port}")
+    int port;
+
     @GetMapping
-    public ApiInfo home(HttpServletRequest request) throws MalformedURLException {
-        return new ApiInfo("1.0", ServerUtils.getURLBase(request) + AppConstants.SWAGGER_ENDPOINT);
+    public ApiInfo home(HttpServletRequest request) {
+        return new ApiInfo("1.0", request.getServerName() + ":" +  port + AppConstants.SWAGGER_ENDPOINT);
     }
 
 
--- src/main/java/com/patrykkosieradzki/todo/backend/service/TodoService.java
+++ src/main/java/com/patrykkosieradzki/todo/backend/service/TodoService.java
@@ -4,6 +4,7 @@ import com.patrykkosieradzki.todo.backend.entity.Todo;
 import com.patrykkosieradzki.todo.backend.entity.User;
 import com.patrykkosieradzki.todo.backend.repository.TodoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
@@ -24,12 +25,13 @@ public class TodoService {
         return todoRepository.findAll();
     }
 
-    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    @PostAuthorize("hasRole('ROLE_ADMIN') or returnObject.user.username == authentication.principal.username")
     public Todo findById(Long id) {
         return todoRepository.findById(id)
                 .orElseThrow(() -> new RuntimeException("Todo not found by id"));
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #todo.user.username == authentication.principal.username")
     public Todo save(Todo todo) {
         todoRepository.save(todo);
         return findById(todo.getId());
@@ -40,15 +42,18 @@ public class TodoService {
         return todoRepository.findAllByUserUsername(username);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #todo.user.username == authentication.principal.username")
     public Todo update(Todo todo) {
         todoRepository.update(todo);
         return findById(todo.getId());
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN') or #user.username == authentication.principal.username")
     public void deleteByDone(User user, boolean done) {
         todoRepository.deleteByDone(user, done);
     }
 
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
     public void deleteById(Long id) {
         todoRepository.deleteById(id);
     }
